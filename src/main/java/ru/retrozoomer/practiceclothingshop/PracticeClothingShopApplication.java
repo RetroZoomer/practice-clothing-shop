e748e6e4e292fddd4695d1186722a84771c6bd8a
RetroZoomer <bayan-herov@mail.ru>
2023-10-26 20:31:22 +0300

    Initial commit

---
+++ src/main/java/ru/retrozoomer/practiceclothingshop/PracticeClothingShopApplication.java
@@ -0,0 +1,13 @@
+package ru.retrozoomer.practiceclothingshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeClothingShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeClothingShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/retrozoomer/practiceclothingshop/PracticeClothingShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.retrozoomer.practiceclothingshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeClothingShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
